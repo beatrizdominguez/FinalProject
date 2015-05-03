89384645e119397230effbcec95727754fc30cac
Beatriz <beatriz.dominguezbd@gmail.com>
2015-05-03 23:44:17 +0200

    Apply styles

--- app/src/main/java/com/closet/beatriz/closet/AdapterItem.java
+++ app/src/main/java/com/closet/beatriz/closet/AdapterItem.java
@@ -13,6 +13,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -101,5 +103,6 @@ public class AdapterItem extends BaseAdapter {
         Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         return bitmap;
     }
+
 
 }
