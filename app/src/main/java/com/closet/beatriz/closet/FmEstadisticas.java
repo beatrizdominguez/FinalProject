787183468d2bf170dffbf0b1f6e2d60670dd1570
Beatriz <beatriz.dominguezbd@gmail.com>
2015-05-05 22:22:57 +0200

    Add price and count statistics.

--- app/src/main/java/com/closet/beatriz/closet/FmEstadisticas.java
+++ app/src/main/java/com/closet/beatriz/closet/FmEstadisticas.java
@@ -3,9 +3,12 @@ package com.closet.beatriz.closet;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by Beatriz on 22/04/2015.
@@ -13,7 +16,7 @@ import android.view.ViewGroup;
 public class FmEstadisticas extends Fragment {
 
     View rootview;
-
+    ItemSQLiteHelper usdbh;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,4 +25,131 @@ public class FmEstadisticas extends Fragment {
         rootview = inflater.inflate(R.layout.fm_estadisticas, container, false);
         return rootview;
     }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onActivityCreated(savedInstanceState);
+
+        //base de datos
+        usdbh = new ItemSQLiteHelper(getActivity(), "Closet",
+                null, 1);
+
+        displayPrices();
+        displayCount();
+
+
+    }
+
+    private void displayPrices() {
+
+        Float[] ItemValue = new Float[10];
+        ItemValue = usdbh.priceStatistics(getActivity());
+        Log.e("TAG-----total value", String.valueOf(ItemValue[0]));
+
+
+        TextView txtTotal = (TextView) rootview.findViewById(R.id.txtTotalValue);
+        TextView shirtValue = (TextView) rootview.findViewById(R.id.txtShirtValue);
+        TextView pantsValue = (TextView) rootview.findViewById(R.id.txtPantstValue);
+        TextView UnderWearValue = (TextView) rootview.findViewById(R.id.txtUnderWearValue);
+        TextView ShoesValue = (TextView) rootview.findViewById(R.id.txtShoesValue);
+        TextView JumperValue = (TextView) rootview.findViewById(R.id.txtJumperValue);
+        TextView PijamasValue = (TextView) rootview.findViewById(R.id.txtPijamasValue);
+        TextView DressValue = (TextView) rootview.findViewById(R.id.txtDressValue);
+        TextView CoatsValue = (TextView) rootview.findViewById(R.id.txtCoatsValue);
+        TextView AccessoriesValue = (TextView) rootview.findViewById(R.id.txtAccesoriesValue);
+
+        Float total;
+        float totalValue = 0;
+        float valueShirts = 0;
+        float valuePants = 0;
+        float valueUnderWear = 0;
+        float valueShoes = 0;
+        float valueJumper = 0;
+        float valuePijamas = 0;
+        float valueDress = 0;
+        float valueCoats = 0;
+        float valueAccessories = 0;
+
+        total = ItemValue[0];
+        valueShirts = ItemValue[1];
+        valuePants = ItemValue[2];
+        valueUnderWear = ItemValue[3];
+        valueShoes = ItemValue[4];
+        valueJumper = ItemValue[5];
+        valuePijamas = ItemValue[6];
+        valueDress = ItemValue[7];
+        valueCoats = ItemValue[8];
+        valueAccessories = ItemValue[9];
+
+        txtTotal.setText(txtTotal.getText() + String.valueOf(total) + " €");
+        shirtValue.setText(String.valueOf(valueShirts) + " €");
+        pantsValue.setText(String.valueOf(valuePants) + " €");
+        UnderWearValue.setText(String.valueOf(valueUnderWear) + " €");
+        ShoesValue.setText(String.valueOf(valueShoes) + " €");
+        JumperValue.setText(String.valueOf(valueJumper) + " €");
+        PijamasValue.setText(String.valueOf(valuePijamas) + " €");
+        DressValue.setText(String.valueOf(valueDress) + " €");
+        CoatsValue.setText(String.valueOf(valueCoats) + " €");
+        AccessoriesValue.setText(String.valueOf(valueAccessories) + " €");
+
+
+    }
+
+    private void displayCount() {
+
+        Integer[] ItemCount = new Integer[10];
+        ItemCount = usdbh.countStatistics(getActivity());
+        Log.e("TAG-----total value", String.valueOf(ItemCount[0]));
+
+
+        TextView totalCount = (TextView) rootview.findViewById(R.id.txtTotalCount);
+        TextView shirtCount = (TextView) rootview.findViewById(R.id.txtShirtCount);
+        TextView pantsCount = (TextView) rootview.findViewById(R.id.txtPantsCount);
+        TextView UnderWearCount = (TextView) rootview.findViewById(R.id.txtUnderWearCount);
+        TextView ShoesCount = (TextView) rootview.findViewById(R.id.txtShoesCount);
+        TextView JumperCount = (TextView) rootview.findViewById(R.id.txtJumperCount);
+        TextView PijamasCount = (TextView) rootview.findViewById(R.id.txtPijamasCount);
+        TextView DressCount = (TextView) rootview.findViewById(R.id.txtDressCount);
+        TextView CoatsCount = (TextView) rootview.findViewById(R.id.txtCoatsCount);
+        TextView AccessoriesCount = (TextView) rootview.findViewById(R.id.txtAccesoriesCount);
+
+        int total;
+        int totalValue = 0;
+        int countShirts = 0;
+        int countPants = 0;
+        int countUnderWear = 0;
+        int valueShoes = 0;
+        int countJumper = 0;
+        int countPijamas = 0;
+        int countDress = 0;
+        int countCoats = 0;
+        int countAccessories = 0;
+
+        total = ItemCount[0];
+        countShirts = ItemCount[1];
+        countPants = ItemCount[2];
+        countUnderWear = ItemCount[3];
+        valueShoes = ItemCount[4];
+        countJumper = ItemCount[5];
+        countPijamas = ItemCount[6];
+        countDress = ItemCount[7];
+        countCoats = ItemCount[8];
+        countAccessories = ItemCount[9];
+
+        totalCount.setText(totalCount.getText() + String.valueOf(total));
+        shirtCount.setText(String.valueOf(countShirts));
+        pantsCount.setText(String.valueOf(countPants));
+        UnderWearCount.setText(String.valueOf(countUnderWear));
+        ShoesCount.setText(String.valueOf(valueShoes));
+        JumperCount.setText(String.valueOf(countJumper));
+        PijamasCount.setText(String.valueOf(countPijamas));
+        DressCount.setText(String.valueOf(countDress));
+        CoatsCount.setText(String.valueOf(countCoats));
+        AccessoriesCount.setText(String.valueOf(countAccessories));
+
+
+    }
+
 }
--- app/src/main/java/com/closet/beatriz/closet/ItemSQLiteHelper.java
+++ app/src/main/java/com/closet/beatriz/closet/ItemSQLiteHelper.java
@@ -73,7 +73,7 @@ public class ItemSQLiteHelper extends SQLiteOpenHelper {
 
                 // // cargamos la información en el objeto
                 int cId = c.getInt(idIdx);
-                 String cFoto = c.getString(fotoIdx);
+                String cFoto = c.getString(fotoIdx);
                 String cDescription = c.getString(descriptionIdx);
                 String cCategory = c.getString(categoryIdx);
                 String cSize = c.getString(sizeIdx);
@@ -105,7 +105,7 @@ public class ItemSQLiteHelper extends SQLiteOpenHelper {
 
     public void guardarItem(Item i) {
 
-       // Log.e("-----guardar item helper", i.getImage());
+        // Log.e("-----guardar item helper", i.getImage());
         SQLiteDatabase db = this.getReadableDatabase();
 
         String sqlInertItem = "INSERT INTO Items (image, description, category, i_size, s_date, price, shop ) VALUES ('" +
@@ -167,6 +167,218 @@ public class ItemSQLiteHelper extends SQLiteOpenHelper {
         String borrarTodas = "DELETE FROM Items";
         database.execSQL(borrarTodas);
 
+    }
+
+    public Float[] priceStatistics(Context context) {
+
+
+        //Log.e("TAG-----------", "en el helper cargarlista");
+
+        Item i;
+
+        float totalValue = 0;
+        float valueShirts = 0;
+        float valuePants = 0;
+        float valueUnderWear = 0;
+        float valueShoes = 0;
+        float valueJumper = 0;
+        float valuePijamas = 0;
+        float valueDress = 0;
+        float valueCoats = 0;
+        float valueAccessories = 0;
+
+        //string of num of categories +1
+        Float[] ItemValue = new Float[10];
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM Items";
+        Cursor c = db.rawQuery(selectQuery, null);
+
+
+        int idIdx = c.getColumnIndex("id");
+        int fotoIdx = c.getColumnIndex("image");
+        int descriptionIdx = c.getColumnIndex("description");
+        int categoryIdx = c.getColumnIndex("category");
+        int sizeIdx = c.getColumnIndex("i_size");
+        int dateIdx = c.getColumnIndex("s_date");
+        int priceIdx = c.getColumnIndex("price");
+        int shopIdx = c.getColumnIndex("shop");
+
+        //Log.e("TAG", "id" + idIdx);
+
+        if (c.moveToFirst()) {
+            do {
+
+                // // cargamos la información en el objeto
+                int cId = c.getInt(idIdx);
+                String cFoto = c.getString(fotoIdx);
+                String cDescription = c.getString(descriptionIdx);
+                String cCategory = c.getString(categoryIdx);
+                String cSize = c.getString(sizeIdx);
+                String cDate = c.getString(dateIdx);
+                Float cPrice = c.getFloat(priceIdx);
+                String cShop = c.getString(shopIdx);
+                //Log.e("foto-helper-cargar", cFoto);
+
+
+                i = new Item(cId, cFoto, cDescription, cCategory, cDate, cSize,
+                        cPrice, cShop);
+                totalValue += i.getPrize();
+
+                Log.e("--------cat", context.getResources().getString(R.string.catCoats));
+                // cargamos la informavión en la lista
+                if (cCategory.equals(context.getResources().getString(R.string.catShirts))) {
+                    valueShirts += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catPants))) {
+                    valuePants += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catUnderWear))) {
+                    valueUnderWear += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catCoats))) {
+                    valueCoats += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catShoes))) {
+                    valueShoes += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catJumper))) {
+                    valueJumper += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catPijamas))) {
+                    valuePijamas += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catDress))) {
+                    valueDress += i.getPrize();
+                } else if (cCategory.equals(context.getResources().getString(R.string.catAccesories))) {
+                    valueAccessories += i.getPrize();
+                }
+                //AÑADIR LAS QUE QUEDARN!!!!!!!!!!!!!!!
+
+            } while (c.moveToNext());
+        }
+
+        ItemValue[0] = totalValue;
+        ItemValue[1] = valueShirts;
+        ItemValue[2] = valuePants;
+        ItemValue[3] = valueUnderWear;
+        ItemValue[4] = valueShoes;
+        ItemValue[5] = valueJumper;
+        ItemValue[6] = valuePijamas;
+        ItemValue[7] = valueDress;
+        ItemValue[8] = valueCoats;
+        ItemValue[9] = valueAccessories;
+        //etc add all categories
+
+        c.close();
+        db.close();
+
+        return ItemValue;
+    }
+
+    public Integer[] countStatistics(Context context) {
+
+
+        //Log.e("TAG-----------", "en el helper cargarlista");
+
+        Item i;
+
+        int totalCount = 0;
+        int countShirts = 0;
+        int countPants = 0;
+        int countUnderWear = 0;
+        int countShoes = 0;
+        int countJumper = 0;
+        int countPijamas = 0;
+        int countDress = 0;
+        int counteCoats = 0;
+        int countAccessories = 0;
+
+        //string of num of categories +1
+        Integer[] ItemCount = new Integer[10];
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM Items";
+        Cursor c = db.rawQuery(selectQuery, null);
+
+
+        int idIdx = c.getColumnIndex("id");
+        int fotoIdx = c.getColumnIndex("image");
+        int descriptionIdx = c.getColumnIndex("description");
+        int categoryIdx = c.getColumnIndex("category");
+        int sizeIdx = c.getColumnIndex("i_size");
+        int dateIdx = c.getColumnIndex("s_date");
+        int priceIdx = c.getColumnIndex("price");
+        int shopIdx = c.getColumnIndex("shop");
+
+        //Log.e("TAG", "id" + idIdx);
+
+        if (c.moveToFirst()) {
+            do {
+
+                // // cargamos la información en el objeto
+                int cId = c.getInt(idIdx);
+                String cFoto = c.getString(fotoIdx);
+                String cDescription = c.getString(descriptionIdx);
+                String cCategory = c.getString(categoryIdx);
+                String cSize = c.getString(sizeIdx);
+                String cDate = c.getString(dateIdx);
+                Float cPrice = c.getFloat(priceIdx);
+                String cShop = c.getString(shopIdx);
+                //Log.e("foto-helper-cargar", cFoto);
+
+
+                i = new Item(cId, cFoto, cDescription, cCategory, cDate, cSize,
+                        cPrice, cShop);
+                totalCount++;
+
+                Log.e("--------cat", context.getResources().getString(R.string.catCoats));
+                // cargamos la informavión en la lista
+                if (cCategory.equals(context.getResources().getString(R.string.catShirts))) {
+
+                    countShirts++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catPants))) {
+
+                    countPants++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catUnderWear))) {
+
+                    countUnderWear++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catCoats))) {
+
+                    counteCoats++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catShoes))) {
+
+                    countShoes++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catJumper))) {
+
+                    countJumper++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catPijamas))) {
+
+                    countPijamas++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catDress))) {
+
+                    countDress++;
+                } else if (cCategory.equals(context.getResources().getString(R.string.catAccesories))) {
+
+                    countAccessories++;
+                }
+                //AÑADIR LAS QUE QUEDARN!!!!!!!!!!!!!!!
+
+            } while (c.moveToNext());
+        }
+
+        ItemCount[0] = totalCount;
+        ItemCount[1] = countShirts;
+        ItemCount[2] = countPants;
+        ItemCount[3] = countUnderWear;
+        ItemCount[4] = countShoes;
+        ItemCount[5] = countJumper;
+        ItemCount[6] = countPijamas;
+        ItemCount[7] = countDress;
+        ItemCount[8] = counteCoats;
+        ItemCount[9] = countAccessories;
+        //etc add all categories
+        Log.e("total number of items", String.valueOf(totalCount));
+
+        c.close();
+        db.close();
+
+        return ItemCount;
     }
 
 }
