74af5119077a0072547abea750ad19f6f41b3721
Beatriz <beatriz.dominguezbd@gmail.com>
2015-05-05 19:42:36 +0200

    Share images

--- app/src/main/java/com/closet/beatriz/closet/AcItemDetail.java
+++ app/src/main/java/com/closet/beatriz/closet/AcItemDetail.java
@@ -1,11 +1,17 @@
 package com.closet.beatriz.closet;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +19,10 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 
@@ -37,7 +47,7 @@ public class AcItemDetail extends Activity {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(AcItemDetail.this, "img grande",  Toast.LENGTH_SHORT).show();
+                Toast.makeText(AcItemDetail.this, "img grande", Toast.LENGTH_SHORT).show();
 
                 Intent intentPhoto = new Intent(AcItemDetail.this, AcItemPhoto.class);
                 Bundle mBundle = new Bundle();
@@ -89,6 +99,16 @@ public class AcItemDetail extends Activity {
                 Toast.makeText(this, "close", Toast.LENGTH_LONG).show();
                 this.finish();
                 break;
+
+            case (R.id.action_share):
+
+              //  Toast.makeText(this, "share", Toast.LENGTH_LONG).show();
+
+                Bitmap bitmap = StringToBitmap(i.getImage());
+
+                shareImage(this, bitmap, "");
+
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -100,6 +120,26 @@ public class AcItemDetail extends Activity {
         decodedString = Base64.decode(image, Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         return bitmap;
+    }
+
+    public static void shareImage(Context context, Bitmap bitmap, String text) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        File f = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
+        try {
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(f));
+        Log.e("TAG", String.valueOf(Uri.fromFile(f)));
+        //share.setPackage("com.whatsapp");
+        context.startActivity(Intent.createChooser(share, "Share!"));
     }
 
 }
--- app/src/main/java/com/closet/beatriz/closet/AcItemPhoto.java
+++ app/src/main/java/com/closet/beatriz/closet/AcItemPhoto.java
@@ -1,14 +1,25 @@
 package com.closet.beatriz.closet;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class AcItemPhoto extends Activity {
@@ -51,6 +62,16 @@ public class AcItemPhoto extends Activity {
 
                 this.finish();
                 break;
+
+            case (R.id.action_share):
+
+                //  Toast.makeText(this, "share", Toast.LENGTH_LONG).show();
+
+                Bitmap bitmap = StringToBitmap(i.getImage());
+
+                shareImage(this, bitmap, "");
+
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -62,5 +83,26 @@ public class AcItemPhoto extends Activity {
         decodedString = Base64.decode(image, Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
         return bitmap;
+    }
+
+
+    public static void shareImage(Context context, Bitmap bitmap, String text) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        File f = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
+        try {
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        share.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(f));
+        Log.e("TAG", String.valueOf(Uri.fromFile(f)));
+        //share.setPackage("com.whatsapp");
+        context.startActivity(Intent.createChooser(share, "Share!"));
     }
 }
